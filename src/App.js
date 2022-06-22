67ed85841790732e9978a5f1835beee38dd02d4e
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-22 19:57:26 +0100

    Update app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Countries from './components/Countries';
 import Filter from './components/Filter';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 import Header from './components/Header';
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1 className='app'> Zahra Countries Map</h1>
+      
       <Header/>
       <Filter />
       <Countries />
