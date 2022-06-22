76483478a9473d92da13ab1c2134048257e3a4ed
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-22 12:47:47 +0100

    Update Countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 //https://restcountries.com/#api-endpoints-v3-all
-const url = "https://restcountries.com/v3.1/all"
+const url = "https://restcountries.com/v2/all";
 const Countries = () => {
 const [countries, setCountries ] = useState([])
+
 
     const fetchCountriesData = async() => {
         const response = await fetch (url)
@@ -15,17 +16,45 @@ fetchCountriesData()
 }, []);
 
 
-
     return (
-        <div>
-            {countries.map ((country) => {
-                const {id} = country
+      <div>
+        {countries.map((country) => {
+          const {
+            name,
+            capital,
+            nativeName,
+            region,
+            flag,
+            population,
+            numericCode,
+            timezones,
+            callingCodes,
 
-                return <article key={id}> Country Data</article>
-            })}
-
-        </div>
+          } = country;
+          return (
+            <article key={numericCode}>
+              <div>
+                <div className="flag">
+                  <img src={flag} alt={name} />
+                </div>
+                <div className="datails">
+                  <div className="Name">
+                    <h2>{name}</h2>
+                    <h3>{nativeName}</h3>
+                    <p>Capital City:</p> <h4>{capital}</h4>
+                  </div>
+                  <p>Region: {region}</p>
+                  <p>population: {population.toLocaleString()}</p>
+                  <p>Calling Code: {callingCodes}</p>
+                  <p>Timezones: {timezones}</p>
+                </div>
+              </div>
+            </article>
+          );
+        })}
+      </div>
     )
-}
+    
+    }
 
 export default Countries 
