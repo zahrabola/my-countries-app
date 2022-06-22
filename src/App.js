4992a3a3d41f8ebdb7c50dc7d69cfeaf02507ed7
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-22 18:05:20 +0100

    Update css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Countries from './components/Countries';
 
+
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-     <h1> Zahra Countries Map</h1>
-       <Countries />
+      <h1> Zahra Countries Map</h1>
+      <Header/>
+      <Countries />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React from "react";
+import { FaMoon } from "react-icons/fa";
+const Header= () => {
 
-const Header = () => {
-    
+return (
+  <header className="header">
+    <div>
+      <h1>Where in the world?</h1>
+    </div>
+    <div>
+      <FaMoon size="1.8em" /> Dark mode
+    </div>
+  </header>
+);
+
 }
-
-
 
 export default Header; 
