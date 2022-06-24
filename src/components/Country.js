fae6508dad20407734f7e61a231887fa80351f5f
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-06-24 14:26:15 +0100

    Update country

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FaArrowLeft} from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
+import "./country.css";
 
 const Country = ( ) => {
   const [country, setCountry] = useState([]);
@@ -29,6 +30,51 @@ const country = await response.json()
           </Link>
         </button>
         <h1> Country Data </h1>
+<section className="country">
+    {country.map((c) => {
+        const {
+          numericCode,
+          flag,
+          name,
+          region,
+          subregion,
+          topLevelDomain,
+          currencies,
+          languages,
+          latlng,
+          nativeName,
+          borders,
+        } = c;
+
+return (
+  <article key={numericCode}>
+    <div className="flag">
+      <img src={flag} alt={name} />
+    </div>
+    <div className="countrydetail">
+      <h2>{name}</h2>
+      <h3 className="native">{nativeName}</h3>
+      <p>Region: {region}</p>
+      <p>Sub Region: {subregion}</p>
+      <p>Top Level Domain: {topLevelDomain}</p>
+      <p>
+        Currency: {currencies[0].symbol}, {currencies[0].name}
+      </p>
+      <p>languages: {languages[0].name}</p>
+      <p>Latitude: {latlng[0]}</p>
+      <p>Longitude: {latlng[1]}</p>
+    </div>
+    <h5>Border Countries:  {borders}</h5>
+  </article>
+);
+
+
+
+
+
+    })}
+</section>
+
       </>
     );
 }
